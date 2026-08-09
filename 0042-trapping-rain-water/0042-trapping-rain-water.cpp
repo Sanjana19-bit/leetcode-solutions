f5c2728c0a1c26dee0af6418b661e40115752c4b
Sanjana Maddheshiya <2k24.cs1n.2413888@gmail.com>
2026-08-08 22:45:06 -0700

    Time: 0 ms (100%), Space: 27.3 MB (17.34%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+          int n=height.size();
+          // previous greatest elements ko nikalo phele
+          vector<int> prev(n);
+          vector<int> next(n);
+          prev[0]= -1;
+          int max = height[0];
+          for(int i=1; i<n; i++){
+            prev[i]=max;
+            if(max<height[i]){
+                max = height[i];
+            }
+          }
+          // next greatest elements ko find karo ab 
+          
+          next[n-1]=-1;
+          max = height[n-1];
+          for(int i=n-2; i>=0; i--){
+            next[i]=max;
+            if(max<height[i]){
+                max=height[i];
+            }
+          }
+          // min dono array me se nikal ke do
+          int mini[n];
+          for(int i=0; i<n; i++){
+            mini[i]= min(prev[i],next[i]);
+          }
+          // CALculating water
+          int water=0;
+          for(int i=1; i<n-1; i++){
+            if(height[i]<mini[i]){
+                water += (mini[i]-height[i]);
+            }
+            
+          }
+          return water;
+        
+    }
+};
