dd66e3431a11e862a868ce1ac03840569fa1a752
Sanjana Maddheshiya <2k24.cs1n.2413888@gmail.com>
2026-08-13 12:22:51 -0700

    Time: 319 ms (38.93%), Space: 113.7 MB (25.51%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,119 @@
+class Solution {
+public:
+
+    struct Node {
+        char leftChar, rightChar;
+        int prefix, suffix, best, len;
+
+        Node() {
+            leftChar = rightChar = '#';
+            prefix = suffix = best = len = 0;
+        }
+
+        Node(char c) {
+            leftChar = rightChar = c;
+            prefix = suffix = best = len = 1;
+        }
+    };
+
+    vector<Node> tree;
+    string s;
+
+    Node merge(Node a, Node b) {
+
+        if (a.len == 0) return b;
+        if (b.len == 0) return a;
+
+        Node res;
+
+        res.len = a.len + b.len;
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        // Prefix
+        res.prefix = a.prefix;
+
+        if (a.prefix == a.len && a.rightChar == b.leftChar) {
+            res.prefix = a.len + b.prefix;
+        }
+
+        // Suffix
+        res.suffix = b.suffix;
+
+        if (b.suffix == b.len && a.rightChar == b.leftChar) {
+            res.suffix = b.len + a.suffix;
+        }
+
+        // Best
+        res.best = max(a.best, b.best);
+
+        // Can join suffix of left + prefix of right
+        if (a.rightChar == b.leftChar) {
+            res.best = max(res.best, a.suffix + b.prefix);
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r) {
+
+        if (l == r) {
+            tree[node] = Node(s[l]);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int idx, char c) {
+
+        if (l == r) {
+            s[idx] = c;
+            tree[node] = Node(c);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid)
+            update(node * 2, l, mid, idx, c);
+        else
+            update(node * 2 + 1, mid + 1, r, idx, c);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    vector<int> longestRepeating(
+        string s,
+        string queryCharacters,
+        vector<int>& queryIndices
+    ) {
+
+        this->s = s;
+
+        int n = s.size();
+
+        tree.resize(4 * n + 5);
+
+        build(1, 0, n - 1);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryIndices.size(); i++) {
+
+            int idx = queryIndices[i];
+            char c = queryCharacters[i];
+
+            update(1, 0, n - 1, idx, c);
+
+            ans.push_back(tree[1].best);
+        }
+
+        return ans;
+    }
+};
