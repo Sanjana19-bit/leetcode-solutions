ae555f9296fd1f4139d66eceb7ed7ceaebc46403
Sanjana Maddheshiya <2k24.cs1n.2413888@gmail.com>
2026-09-18 23:47:48 -0700

    Time: 0 ms (100%), Space: 8.6 MB (77.77%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int n = name.size();
+        int m = typed.size();
+        int i=0;
+        int j =0;
+        while(i<n && j<m){
+            if(name[i]==typed[j]){
+                i++;
+                j++;
+            }
+            else if (i>0 && name[i-1]==typed[j]){
+                j++;
+            }
+            else {
+                return false;
+            }
+        }
+            if(i<n){
+                return false;
+            }
+            while(j<m){
+                if(typed[j] != name[i-1])
+                    return false;
+                j++;
+                
+        }
+        return true;
+    }
+};
